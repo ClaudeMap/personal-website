ebd30099d5584d00f018ca64d7ebf514d652c42b
Claude Mapara <claude.mapara@hotmail.co.uk>
2025-05-29 11:42:57 +0100

    refactor:close menu on link click (mobile only)

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,11 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector(".hamburger");
   const navigationList = document.querySelector(".styled-list");
+  const navLinks = document.querySelectorAll(".styled-list a ");
 
   if (hamburger && navigationList) {
     hamburger.addEventListener("click", () => {
       navigationList.classList.toggle("expanded");
       hamburger.classList.toggle("active");
+    });
+
+    navLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        navigationList.classList.remove("expanded");
+        hamburger.classList.remove("active");
+      });
     });
   }
 });
