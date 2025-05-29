ee0bc03086501f235b9fa159e17e0086984c0d88
Claude Mapara <claude.mapara@hotmail.co.uk>
2025-05-29 15:45:22 +0100

    refactor: menu blur effect and user click events

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,28 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector(".hamburger");
   const navigationList = document.querySelector(".styled-list");
-  const navLinks = document.querySelectorAll(".styled-list a ");
+  const navLinks = document.querySelectorAll(".styled-list a");
+
+  const closeMenu = () => {
+    navigationList.classList.remove("expanded");
+    hamburger.classList.remove("active");
+    document.body.classList.remove("menu-open-blur");
+  };
 
   if (hamburger && navigationList) {
     hamburger.addEventListener("click", () => {
       navigationList.classList.toggle("expanded");
       hamburger.classList.toggle("active");
+      document.body.classList.toggle("menu-open-blur");
     });
 
     navLinks.forEach((link) => {
-      link.addEventListener("click", () => {
-        navigationList.classList.remove("expanded");
-        hamburger.classList.remove("active");
-      });
+      link.addEventListener("click", closeMenu);
     });
 
     document.addEventListener("click", (event) => {
       const isClickInsideMenu = navigationList.contains(event.target);
       const isClickOnHamburger = hamburger.contains(event.target);
 
-      if (!isClickInsideMenu && !isClickOnHamburger) {
-        navigationList.classList.remove("expanded");
-        hamburger.classList.remove("active");
+      if (
+        navigationList.classList.contains("expanded") &&
+        !isClickInsideMenu &&
+        !isClickOnHamburger
+      ) {
+        closeMenu();
       }
     });
   }
